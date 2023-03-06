61f174660d51e3aa06b053d2003d247e0f46cde5
Mudasiru Adewale <50289759+whaletb@users.noreply.github.com>
2023-03-06 21:20:12 +0100

    Update omni_TECNO-KC8.mk

--- omni_TECNO-KC8.mk
+++ omni_TECNO-KC8.mk
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/omni/config/common.mk)
 
 # Inherit from TECNO-KC8 device
-$(call inherit-product, device/tecno/TECNO-KC8/device.mk)
+$(call inherit-product, device/tecno/KC8/device.mk)
 
-PRODUCT_DEVICE := TECNO-KC8
-PRODUCT_NAME := omni_TECNO-KC8
+PRODUCT_DEVICE := KC8
+PRODUCT_NAME := omni_KC8
 PRODUCT_BRAND := TECNO
 PRODUCT_MODEL := TECNO KC8
 PRODUCT_MANUFACTURER := tecno
