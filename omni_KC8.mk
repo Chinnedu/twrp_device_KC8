37feddfc0cc16629704535cdcd4ac8263158571e
infinixs5lite <kelvinchinedu97@gmail.com>
2023-03-06 23:17:54 +0100

    Update omni_KC8.mk

--- omni_KC8.mk
+++ omni_KC8.mk
@@ -7,17 +7,23 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, build/target/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
 
-# Inherit some common Omni stuff.
+# Installs gsi keys into ramdisk, to boot a developer GSI with verified boot.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# Inherit some common omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# Inherit from TECNO-KC8 device
+# Inherit from KC8 device
 $(call inherit-product, device/tecno/KC8/device.mk)
 
 PRODUCT_DEVICE := KC8
 PRODUCT_NAME := omni_KC8
 PRODUCT_BRAND := TECNO
-PRODUCT_MODEL := TECNO KC8
+PRODUCT_MODEL := Tecno Spark 4
 PRODUCT_MANUFACTURER := tecno
 
 PRODUCT_GMS_CLIENTID_BASE := android-transsion-tecno-rev1
